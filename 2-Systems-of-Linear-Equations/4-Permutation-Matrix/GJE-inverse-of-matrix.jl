366fe2c6e4209b6e7ed7dc5deddcfc55f5757627
Krittin Chaowakarn <krittin.chao@gmail.com>
2024-08-23 15:03:30 +0700

    Create GJE-inverse-of-matrix.jl

---
+++ 2-Systems-of-Linear-Equations/4-Permutation-Matrix/GJE-inverse-of-matrix.jl
@@ -0,0 +1,12 @@
+# Gaussian-Jordan Elimination (GJE)
+
+#=
+    The reason why GJE is not aplied to solve linear equation 
+    because of the inaccurate solution from floating-point arithmetic 
+    and the 50% more operation counts requirement.
+
+    Howver, GJE is mathematically elegant is useful in proving many theorems.
+    GJE is also useful in parallel computing.
+=#
+
+# Computing inverses with GEEP
