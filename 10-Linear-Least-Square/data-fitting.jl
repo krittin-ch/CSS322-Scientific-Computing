ad5dac2c1f836cb7e3800c1d47ae821bec1d12a2
krittin-ch <130139478+krittin-ch@users.noreply.github.com>
2024-10-15 22:01:55 +0700

    Update data-fitting.jl

--- 10-Linear-Least-Square/data-fitting.jl
+++ 10-Linear-Least-Square/data-fitting.jl
@@ -39,4 +39,88 @@ Minimizing ||Ax - b||_2
 
 Lemma 1: Suppose A has rank n. Then A^T A is symmetrix positive definite.
 Lemma 2: f(x) = x^T Cx - 2h^T x + d, where X is symmetric positive definite. Then f has a unique minimizer at x* = C^-1 h.
+
+We can conclude that x = (A^T A)^-1 A^T b by using Lemma (1) and (2)
+
+
+Theorem: x = (A^T A)^- A^T b is the unique solution to the linear least squares problem when A has rank n
+
+Method of Normal Equations
+    Solve min_x ||Ax - b||_2 by 
+        1. Form products A^T A and A^T b 
+        2. Solve A^T ax = A^T b using Cholesky factorization
+
+    (Assuming rank(A) = n, Lemma (1) tells us that A^T A is symmetric positive definite)
 =#
+
+function Cholesky_factorization(A::Matrix)
+    n = size(A, 1)
+    L = deepcopy(A)
+
+    for k in 1:n
+        L[k, k] = sqrt(L[k, k])
+        L[k, k+1:n] .= 0
+        
+        for i in k+1:n 
+            L[i, k] = L[i, k] / L[k, k]
+        end
+        
+        for j in k+1:n 
+            for i in k+1:n 
+                L[i, j] -= L[i, k]*L[j, k]
+            end
+        end
+    end
+
+    return L
+end
+
+function forward_substitution(L::Matrix, b::Vector)
+    n = size(L, 1)
+    w = zeros(n)
+
+    for i in 1:n
+        w[i] =  b[i] / L[i, i]
+        for j in i+1:n 
+            b[j] -= L[j, i] * w[i]
+        end
+    end
+
+    return w
+end
+
+function backward_substitution(L_T::Matrix, w::Vector)
+    n = size(L_T, 1)
+    x = zeros(n)
+
+    for i in n:-1:1
+        x[i] = w[i] / L_T[i, i]
+        for j in 1:i-1
+            w[j] -= L_T[j, i] * x[i]
+        end
+    end
+
+    return x
+end
+
+function normal_equation(A::Matrix, b::Vector)
+    L = Cholesky_factorization(transpose(A) * A)
+    L_T = collect(transpose(L))
+
+    w = forward_substitution(L, transpose(A) * b)
+    x = backward_substitution(L_T, w)
+
+    return x
+end
+
+A = Matrix{Float64}([
+    0 1;
+    1 1;
+    2 1;
+    3 1
+])
+
+b = Vector{Float64}([1; -1; 4; 2])
+
+x = normal_equation(A, b)
+
