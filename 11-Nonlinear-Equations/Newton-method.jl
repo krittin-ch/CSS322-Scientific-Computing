3d9fb6acdebbab797585e8fe1e6027840b06d5c1
krittin-ch <130139478+krittin-ch@users.noreply.github.com>
2024-11-04 21:49:27 +0700

    Create Newton-method.jl

---
+++ 11-Nonlinear-Equations/Newton-method.jl
@@ -0,0 +1,59 @@
+# Newton's Method
+
+#=
+    Newton's Method
+        The first two terms of Taylor series give 
+            f(x + h) ≈ f(x) + f'(x)*h
+
+        Suppose we know a point x^(k) that is close to a root.
+        We want to find h such that f(x^(k) + h) = 0 
+        
+        Using the approximation, let us instead find h such that 
+            f(x^(k)) = f'(x^(k))*h = 0
+
+        That is h = -f(x^(k)) / f'(x^(k))
+
+        We need to repeat the process 
+
+            x^(0) = initial guess 
+
+            for k = 0, 1, 2, . . .
+                x^(k + 1) = x^(k) - f(x^(k)) / f'(x^(k))
+            end
+
+        
+    Proporties of Newton's Method
+        If the initial point x^(0) is close enough to a simple root, Newton's method converges quadratically.
+            If too far, it may converge more slowly or may not converge at all.
+
+        Newton's method generally does not converge to a singlar root (i.e. a root x* where f'(x*) = 0)
+        or if it does, it does so very slowly (linear convergence at best).
+=#
+
+f(x) = x^2 - 3*x - 4
+
+function forward_difference(f::Function, x)
+    h = 0.001
+    diff = (f(x + h) - f(x)) / h
+
+    return diff
+end
+
+function Newton_method(f::Function, init_guess)
+    err = 0.01
+
+    x = init_guess
+    h = 1
+
+    while h > err
+        h = f(x) / forward_difference(f, x)
+        x -= h
+    end
+
+    return x
+end
+
+x = Newton_method(f, 5)
+
+println("root x: ", x)
+println("f(x = ", x, "): ", f(x))
